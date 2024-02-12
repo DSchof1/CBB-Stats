5a4152d0c94d09a6619793f8cce2042ad3cd3538
DSchof1 <dodo1672@bell.net>
2024-02-12 16:13:50 -0500

    EST message

--- schedule_download_component.R
+++ schedule_download_component.R
@@ -10,7 +10,7 @@ excel_download <- box(
   fluidRow(
     column(12,
            style="text-align: justify;",
-           "A nice layout is coming, but for now this will allow you to download the schedule in a usable excel format.  If you are downloading a day very far out it may take a few seconds to open the download dialog box as the data is only pre-loaded 4 days out.")
+           "Please note game times are currently only EST.  A nice layout is coming, but for now this will allow you to download the schedule in a usable excel format.  If you are downloading a day very far out it may take a few seconds to open the download dialog box as the data is only pre-loaded 4 days out.")
   ),
   fluidRow(id = "excel_dl_button",
     column(8,
