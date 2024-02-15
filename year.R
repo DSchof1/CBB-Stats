6452f5076255f23cea839b685c1d5d3b06fdee1b
DSchof1 <dodo1672@bell.net>
2024-02-15 11:06:40 -0500

    Added excluded dates

--- year.R
+++ year.R
@@ -1,4 +1,5 @@
 #Current season year (year when the championship will happen)
 champ_year <- 2024
+exclude_dates <- c("2023-12-25", "2023-12-26")
 first_day_of_games <- "2023-11-06"
 last_day_of_games <- "2024-04-08"
