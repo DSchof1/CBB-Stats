b2dc62614eeafa60d8aeb8afdf51942854d5fe9b
DSchof1 <dodo1672@bell.net>
2026-03-06 10:51:22 -0500

    Timezone schedule fix

--- Data/year.R
+++ Data/year.R
@@ -8,22 +8,22 @@ previous_year_first_day_of_games <- "2024-11-04"
 last_day_of_games <- "2026-04-06"
 last_year_last_day_of_games <- "2025-04-07"
 
-if(((as.Date(with_tz(Sys.time(),tzone = "EST")) > last_year_last_day_of_games)
-   & (as.Date(with_tz(Sys.time(),tzone = "EST")) < paste0((champ_year-1),"-10-20")))
-   | (as.Date(with_tz(Sys.time(),tzone = "EST")) > last_day_of_games)){
+if(((as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") > last_year_last_day_of_games)
+   & (as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") < paste0((champ_year-1),"-10-20")))
+   | (as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") > last_day_of_games)){
   preseason <- FALSE
   offseason <- TRUE
-} else if((as.Date(with_tz(Sys.time(),tzone = "EST")) >= paste0((champ_year-1),"-10-20"))
-          & (as.Date(with_tz(Sys.time(),tzone = "EST")) < first_day_of_games)){
+} else if((as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") >= paste0((champ_year-1),"-10-20"))
+          & (as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") < first_day_of_games)){
   preseason <- TRUE
   offseason <- FALSE
-} else if((as.Date(with_tz(Sys.time(),tzone = "EST")) >= first_day_of_games)
-          & (as.Date(with_tz(Sys.time(),tzone = "EST")) <= last_day_of_games)){
+} else if((as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") >= first_day_of_games)
+          & (as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") <= last_day_of_games)){
   preseason <- FALSE
   offseason <- FALSE
 }
 
-if((as.Date(with_tz(Sys.time(),tzone = "EST")) > last_year_last_day_of_games)
-   & (as.Date(with_tz(Sys.time(),tzone = "EST")) < paste0((champ_year-1),"-10-20"))){
+if((as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") > last_year_last_day_of_games)
+   & (as.Date(with_tz(Sys.time(),tzone = "America/Toronto"), tz = "America/Toronto") < paste0((champ_year-1),"-10-20"))){
   champ_year <- champ_year-1
 }
